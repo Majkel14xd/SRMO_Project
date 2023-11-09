e2cd3c45b1bc7f37d079065fafe901bf9e7b2523
Majkel14xd <mlidwa@wp.pl>
2023-11-10 00:37:22 +0100

    images conver and print classes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow.keras import models, layers, callbacks
 
 dataset = tf.keras.utils.image_dataset_from_directory(
-    directory  = ".\images", 
+    directory  = "./images_jpg", 
     color_mode = 'rgb',     
     batch_size = 32,
     image_size = (256, 256),  
