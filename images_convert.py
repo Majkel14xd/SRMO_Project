7523e95f5f7d3b70670747f1cb10e9b745166954
Majkel14xd <mlidwa@wp.pl>
2023-11-10 00:40:03 +0100

    fix

--- images_convert.py
+++ images_convert.py
@@ -2,31 +2,20 @@ from PIL import Image
 import os
 
 def convert_tif_to_jpg(input_folder, output_folder):
-
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-
-
     for root, dirs, files in os.walk(input_folder):
         for file in files:
- 
             if file.lower().endswith(".tif"):
-                tif_path = os.path.join(root, file)
-                
-        
+                tif_path = os.path.join(root, file)           
                 tif_image = Image.open(tif_path)
-                
-    
                 jpg_path = os.path.join(output_folder, os.path.relpath(tif_path, input_folder)).replace(".tif", ".jpg")
-                
                 jpg_folder = os.path.dirname(jpg_path)
                 if not os.path.exists(jpg_folder):
                     os.makedirs(jpg_folder)
-                
                 tif_image.convert("RGB").save(jpg_path, "JPEG")
                 print(f"Skonwertowano: {tif_path} -> {jpg_path}")
 
 input_folder_path = "images"
 output_folder_path = "images_jpg"
-
 convert_tif_to_jpg(input_folder_path, output_folder_path)
