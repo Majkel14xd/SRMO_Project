561aded3ce9059d543868cea7c1a9b697e15f8ff
Majkel14xd <mlidwa@wp.pl>
2023-11-10 00:29:11 +0100

    image import to dataset

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import cv2
 import tensorflow as tf
+from tensorflow.keras import models, layers, callbacks
 
-print("testtest2")
+dataset = tf.keras.utils.image_dataset_from_directory(
+    directory  = ".\images", 
+    color_mode = 'rgb',     
+    batch_size = 32,
+    image_size = (256, 256),  
+    shuffle    = True,
+    seed       = None,
+    crop_to_aspect_ratio = False,
+)
 
+
+class_names = dataset.class_names
+print(class_names)
